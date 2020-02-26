0889a80cf2836c990c35ff8bda0592d272dd999a
Parag Singh <psparag1997@gmail.com>
2020-02-26 14:52:13 +0530

    Commit 26-Feb-2020 (Parag -03)

--- dist/script.js
+++ dist/script.js
@@ -1,92 +1,88 @@
 //get scrollbar width
 document.documentElement.style.setProperty("--scrollbar-width", window.innerWidth - document.documentElement.clientWidth + "px");
 
-
 /*
-*Smooth page scroll
-*
-*/
-
+ *Smooth page scroll
+ *
+ */
 
 function getAbsoluteHeight(el) {
-  // Get the DOM Node if you pass in a string
-  el = (typeof el === 'string') ? document.querySelector(el) : el; 
+    // Get the DOM Node if you pass in a string
+    el = typeof el === "string" ? document.querySelector(el) : el;
 
-  var styles = window.getComputedStyle(el);
-  var margin = parseFloat(styles['marginTop']) +
-               parseFloat(styles['marginBottom']);
+    var styles = window.getComputedStyle(el);
+    var margin = parseFloat(styles["marginTop"]) + parseFloat(styles["marginBottom"]);
 
-  return Math.ceil(el.offsetHeight + margin);
+    return Math.ceil(el.offsetHeight + margin);
 }
 
 var html = document.documentElement;
 var body = document.body;
 var el = document.querySelector(".content");
-var marginTop = parseInt(window.getComputedStyle(el).getPropertyValue('margin-top'));
+var marginTop = parseInt(window.getComputedStyle(el).getPropertyValue("margin-top"));
 console.log(marginTop);
 
 var scroller = {
-  target: document.querySelector("#scroll-container"),
-  ease: 0.05, // <= scroll speed
-  endY: 0,
-  y: 0,
-  resizeRequest: 1,
-  scrollRequest: 0,
+    target: document.querySelector("#scroll-container"),
+    ease: 0.05, // <= scroll speed
+    endY: 0,
+    y: 0,
+    resizeRequest: 1,
+    scrollRequest: 0
 };
 
 var requestId = null;
 
 TweenLite.set(scroller.target, {
-  rotation: 0.01,
-  force3D: true
+    rotation: 0.01,
+    force3D: true
 });
 
 window.addEventListener("load", onLoad);
 
-function onLoad() {    
-  updateScroller();  
-  window.focus();
-  window.addEventListener("resize", onResize);
-  document.addEventListener("scroll", onScroll); 
+function onLoad() {
+    updateScroller();
+    window.focus();
+    window.addEventListener("resize", onResize);
+    document.addEventListener("scroll", onScroll);
 }
 
 function updateScroller() {
-  
-  var resized = scroller.resizeRequest > 0;
-    
-  if (resized) {    
-    var height = scroller.target.clientHeight + marginTop;
-    body.style.height = height + "px";
-    scroller.resizeRequest = 0;
-  }
-      
-  var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+    var resized = scroller.resizeRequest > 0;
 
-  scroller.endY = scrollY;
-  scroller.y += (scrollY - scroller.y) * scroller.ease;
+    if (resized) {
+        var height = scroller.target.clientHeight + marginTop;
+        body.style.height = height + "px";
+        scroller.resizeRequest = 0;
+    }
 
-  if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
-    scroller.y = scrollY;
-    scroller.scrollRequest = 0;
-  }
-  
-  TweenLite.set(scroller.target, { 
-    y: -scroller.y 
-  });
-  
-  requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+    var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+
+    scroller.endY = scrollY;
+    scroller.y += (scrollY - scroller.y) * scroller.ease;
+
+    if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+        scroller.y = scrollY;
+        scroller.scrollRequest = 0;
+    }
+
+    TweenLite.set(scroller.target, {
+        y: -scroller.y
+    });
+
+    requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
 }
 
 function onScroll() {
-  scroller.scrollRequest++;
-  if (!requestId) {
-    requestId = requestAnimationFrame(updateScroller);
-  }
+    scroller.scrollRequest++;
+    if (!requestId) {
+        requestId = requestAnimationFrame(updateScroller);
+    }
 }
 
 function onResize() {
-  scroller.resizeRequest++;
-  if (!requestId) {
-    requestId = requestAnimationFrame(updateScroller);
-  }
+    scroller.resizeRequest++;
+    if (!requestId) {
+        requestId = requestAnimationFrame(updateScroller);
+    }
 }
