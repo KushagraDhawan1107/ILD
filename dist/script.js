59c40c3c0bcd1e2f30e98855f532210b6e2a0672
raahul100 <38454354+raahul100@users.noreply.github.com>
2020-02-26 11:55:49 +0530

    add smooth scroll
    added by rahul

--- dist/script.js
+++ dist/script.js
@@ -1,40 +1,76 @@
 //get scrollbar width
 document.documentElement.style.setProperty("--scrollbar-width", window.innerWidth - document.documentElement.clientWidth + "px");
 
-/*---------------
-Virtual smooth Scroll
----------------*/
-class VirtualSmoothScroll {
-    constructor() {
-        this.$ = {
-            dom: document.getElementsByClassName("ild-section")[0],
-            fakeScroll: document.getElementsByClassName("ild-section__outer-scroll")[0],
-            scroll: document.getElementsByClassName("ild-section__scroll")[0]
-        };
 
-        this.init();
-        this.events();
-        this.animate();
-    }
+/*
+*Smooth page scroll
+*
+*/
+var html = document.documentElement;
+var body = document.body;
 
-    init() {
-        const winH = window.innerHeight;
-        document.body.style.height = `${winH}px`;
-        this.$.dom.style.height = `${winH}px`;
-        this.$.fakeScroll.style.height = `${this.$.scroll.getBoundingClientRect().height}px`;
-    }
+var scroller = {
+  target: document.querySelector("#scroll-container"),
+  ease: 0.05, // <= scroll speed
+  endY: 0,
+  y: 0,
+  resizeRequest: 1,
+  scrollRequest: 0,
+};
 
-    animate() {
-        requestAnimationFrame(this.animate.bind(this));
-        TweenMax.to(this.$.scroll, 1, {
-            y: -this.$.dom.scrollTop
-        });
-    }
+var requestId = null;
 
-    events() {
-        window.addEventListener("load", this.init.bind(this));
-        window.addEventListener("resize", this.init.bind(this));
-    }
+TweenLite.set(scroller.target, {
+  rotation: 0.01,
+  force3D: true
+});
+
+window.addEventListener("load", onLoad);
+
+function onLoad() {    
+  updateScroller();  
+  window.focus();
+  window.addEventListener("resize", onResize);
+  document.addEventListener("scroll", onScroll); 
 }
 
-const vs = new VirtualSmoothScroll();
+function updateScroller() {
+  
+  var resized = scroller.resizeRequest > 0;
+    
+  if (resized) {    
+    var height = scroller.target.clientHeight;
+    body.style.height = height + "px";
+    scroller.resizeRequest = 0;
+  }
+      
+  var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+
+  scroller.endY = scrollY;
+  scroller.y += (scrollY - scroller.y) * scroller.ease;
+
+  if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+    scroller.y = scrollY;
+    scroller.scrollRequest = 0;
+  }
+  
+  TweenLite.set(scroller.target, { 
+    y: -scroller.y 
+  });
+  
+  requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+}
+
+function onScroll() {
+  scroller.scrollRequest++;
+  if (!requestId) {
+    requestId = requestAnimationFrame(updateScroller);
+  }
+}
+
+function onResize() {
+  scroller.resizeRequest++;
+  if (!requestId) {
+    requestId = requestAnimationFrame(updateScroller);
+  }
+}
