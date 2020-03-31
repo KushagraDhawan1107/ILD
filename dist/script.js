fc424f65907d211cda183bc2f4f97cfc94a039b8
raahul100 <38454354+raahul100@users.noreply.github.com>
2020-03-31 10:36:46 +0530

    add horizontal scroll on about us page

--- dist/script.js
+++ dist/script.js
@@ -6,85 +6,85 @@ document.documentElement.style.setProperty("--scrollbar-width", window.innerWidt
  *
  */
 
-function getAbsoluteHeight(el) {
-	// Get the DOM Node if you pass in a string
-	el = typeof el === "string" ? document.querySelector(el) : el;
+// function getAbsoluteHeight(el) {
+// 	// Get the DOM Node if you pass in a string
+// 	el = typeof el === "string" ? document.querySelector(el) : el;
 
-	var styles = window.getComputedStyle(el);
-	var margin = parseFloat(styles["marginTop"]) + parseFloat(styles["marginBottom"]);
+// 	var styles = window.getComputedStyle(el);
+// 	var margin = parseFloat(styles["marginTop"]) + parseFloat(styles["marginBottom"]);
 
-	return Math.ceil(el.offsetHeight + margin);
-}
+// 	return Math.ceil(el.offsetHeight + margin);
+// }
 
-var html = document.documentElement;
-var body = document.body;
-var el = document.querySelector(".content");
-var marginTop = parseInt(window.getComputedStyle(el).getPropertyValue("margin-top"));
+// var html = document.documentElement;
+// var body = document.body;
+// var el = document.querySelector(".content");
+// var marginTop = parseInt(window.getComputedStyle(el).getPropertyValue("margin-top"));
 
-var scroller = {
-	target: document.querySelector("#scroll-container"),
-	ease: 0.05, // <= scroll speed
-	endY: 0,
-	y: 0,
-	resizeRequest: 1,
-	scrollRequest: 0,
-};
+// var scroller = {
+// 	target: document.querySelector("#scroll-container"),
+// 	ease: 0.05, // <= scroll speed
+// 	endY: 0,
+// 	y: 0,
+// 	resizeRequest: 1,
+// 	scrollRequest: 0,
+// };
 
-var requestId = null;
+// var requestId = null;
 
-TweenLite.set(scroller.target, {
-	rotation: 0.01,
-	force3D: true,
-});
+// TweenLite.set(scroller.target, {
+// 	rotation: 0.01,
+// 	force3D: true,
+// });
 
-window.addEventListener("load", onLoad);
+// window.addEventListener("load", onLoad);
 
-function onLoad() {
-	updateScroller();
-	window.focus();
-	window.addEventListener("resize", onResize);
-	document.addEventListener("scroll", onScroll);
-}
+// function onLoad() {
+// 	updateScroller();
+// 	window.focus();
+// 	window.addEventListener("resize", onResize);
+// 	document.addEventListener("scroll", onScroll);
+// }
 
-function updateScroller() {
-	var resized = scroller.resizeRequest > 0;
+// function updateScroller() {
+// 	var resized = scroller.resizeRequest > 0;
 
-	if (resized) {
-		var height = scroller.target.clientHeight + marginTop;
-		body.style.height = height + "px";
-		scroller.resizeRequest = 0;
-	}
+// 	if (resized) {
+// 		var height = scroller.target.clientHeight + marginTop;
+// 		body.style.height = height + "px";
+// 		scroller.resizeRequest = 0;
+// 	}
 
-	var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+// 	var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
 
-	scroller.endY = scrollY;
-	scroller.y += (scrollY - scroller.y) * scroller.ease;
+// 	scroller.endY = scrollY;
+// 	scroller.y += (scrollY - scroller.y) * scroller.ease;
 
-	if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
-		scroller.y = scrollY;
-		scroller.scrollRequest = 0;
-	}
+// 	if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+// 		scroller.y = scrollY;
+// 		scroller.scrollRequest = 0;
+// 	}
 
-	TweenLite.set(scroller.target, {
-		y: -scroller.y,
-	});
+// 	TweenLite.set(scroller.target, {
+// 		y: -scroller.y,
+// 	});
 
-	requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
-}
+// 	requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+// }
 
-function onScroll() {
-	scroller.scrollRequest++;
-	if (!requestId) {
-		requestId = requestAnimationFrame(updateScroller);
-	}
-}
+// function onScroll() {
+// 	scroller.scrollRequest++;
+// 	if (!requestId) {
+// 		requestId = requestAnimationFrame(updateScroller);
+// 	}
+// }
 
-function onResize() {
-	scroller.resizeRequest++;
-	if (!requestId) {
-		requestId = requestAnimationFrame(updateScroller);
-	}
-}
+// function onResize() {
+// 	scroller.resizeRequest++;
+// 	if (!requestId) {
+// 		requestId = requestAnimationFrame(updateScroller);
+// 	}
+// }
 
 /** button code */
 
@@ -156,4 +156,39 @@ temp_button.forEach((e) => {
 	e.addEventListener("mouseleave", () => {
 		button_animation_reverse();
 	});
+});
+
+
+
+//Make a ScrollMagic scene
+var controller = new ScrollMagic.Controller();
+var height = window.innerHeight;
+var offsetValue = -height / 2;
+var horscrgallery = document.querySelectorAll(".about-us-stats-section")
+
+horscrgallery.forEach( function(e) {
+
+  //References to DOM elements
+  var imageOverlay = e.querySelector(".about-us-stats-section-container--grid");
+  var containerwidth = imageOverlay.offsetWidth + "px";
+  console.log(containerwidth);
+  
+
+  var animateImage =  new gsap.timeline();
+
+  animateImage.to(imageOverlay, 6,   {x: "-20%" })
+  .to(imageOverlay, 6,   {x: "-40%" })
+  .to(imageOverlay, 6,   {x: "-60%" })
+  .to(imageOverlay, 6,   {x: "-80%" });
+
+  //Make a ScrollMagic scene
+  var scene = new ScrollMagic.Scene({
+    triggerElement: e,
+	triggerHook: 0,
+    duration: "400%",
+  })
+  .setTween(animateImage)
+  .setPin(e)
+	.addIndicators()
+    .addTo(controller);
 });
